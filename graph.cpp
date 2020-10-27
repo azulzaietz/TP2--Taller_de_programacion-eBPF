496b64ea4e00a27eefca69cf07e4546f8bb48641
azulzaietz <azulzaietz@gmail.com>
2020-10-26 21:53:06 -0300

    faltaba un this

--- graph.cpp
+++ graph.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 
 void Graph:: add_node(std::string instruction) {
-    if (!nodes.empty()) {
+    if (!this->nodes.empty()) {
         Node* previous_node = this->nodes.back();
         this->nodes.push_back(new Node(instruction));
         if (previous_node->add_next()) {
