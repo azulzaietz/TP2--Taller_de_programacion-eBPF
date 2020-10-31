aa1dbdaae1efa0178523290784016ffab2a71e69
azulzaietz <azulzaietz@gmail.com>
2020-10-31 17:19:53 -0300

    main hilos

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include "eBPF.h"
+#include "threadEbpfStarter.h"
 
-int main () {
-   eBPF ebpf;
-   ebpf.read_code();
-   //ebpf.print_grafo();
-   ebpf.verify();
+int main (int argc, const char* argv[]) {
+   if (argc < 3) return -1;
+
+   ThreadEbpfStarter th_ebpf_starter(argc, argv);
+   th_ebpf_starter.start();
+   
    return 0;
 }
