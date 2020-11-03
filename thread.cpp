b12f91a5f194c56ff07eb71531d988691bc77edd
azulzaietz <azulzaietz@gmail.com>
2020-11-03 15:58:51 -0300

    Revert "-unused methods"
    This reverts commit d9f546e197485d994b4aa0d2fd4e8361a594ecd0.
    
    O

--- thread.cpp
+++ thread.cpp
@@ -11,4 +11,13 @@ void Thread::join() {
     this->thread.join();
 }
 
+Thread::Thread(Thread &&other) {
+    this->thread = std::move(other.thread);
+}
+
+Thread &Thread::operator = (Thread &&other) {
+    this->thread = std::move(other.thread);
+    return *this;
+}
+
 Thread::~Thread() {}
